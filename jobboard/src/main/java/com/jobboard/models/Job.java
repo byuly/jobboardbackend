4f66927743b4c0c5a20ae4024e28984a42093a9e
Byeori KIm <bk.byeori.kim@gmail.com>
2025-02-22 22:25:56 -0800

    postgres and model

---
+++ jobboard/src/main/java/com/jobboard/models/Job.java
@@ -0,0 +1,26 @@
+package com.jobboard.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String company;
+    private String location;
+    private String description;
+    private String type; // Full-time, Part-time, Intern
+}
