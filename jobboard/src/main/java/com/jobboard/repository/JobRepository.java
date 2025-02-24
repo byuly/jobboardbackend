5602bc6f08065a9a879117c1f468fbebefc61362
Byeori KIm <bk.byeori.kim@gmail.com>
2025-02-23 17:20:11 -0800

    repositories for Job and Company

---
+++ jobboard/src/main/java/com/jobboard/repository/JobRepository.java
@@ -0,0 +1,12 @@
+package com.jobboard.repository;
+
+import com.jobboard.models.Job;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface JobRepository extends JpaRepository<Job, Long> {
+    List<Job> findByLocation(String location);
+}
